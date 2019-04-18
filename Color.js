9e185a94775b27699c0961801fe02b37a7fbe222
abufaris <abufaris@MacBook-Air-alkhas-b-abufaris.local>
2019-04-18 09:27:06 +0300

    Reem Final Project

---
+++ Color.js
@@ -0,0 +1,34 @@
+$('#PINK').on('click', function(){
+  $('body').css('background-color','pink');
+})
+
+
+$('#RED').on('click', function(){
+  $('body').css('background-color','RED');
+})
+
+
+$('#BLUE').on('click', function(){
+  $('body').css('background-color','BLUE');
+})
+
+
+$('#YELLOW').on('click', function(){
+  $('body').css('background-color','YELLOW');
+})
+
+$('#ORANGE').on('click', function(){
+  $('body').css('background-color','ORANGE');
+})
+
+$('#GREEN').on('click', function(){
+  $('body').css('background-color','GREEN');
+})
+
+$('#BLACK').on('click', function(){
+  $('body').css('background-color','BLACK');
+})
+
+$('#WHITE').on('click', function(){
+  $('body').css('background-color','WHITE');
+})
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function showMessage(your name)
-{
-    var yourname = prompt ("Enter Your Name","For example: Reem, Ibrahim,...")
-}
+$(document).ready(function(){
+ 
+  $('#name-form').on('submit',changeHeader);
 
+  function changeHeader(event){
+    event.preventDefault();
+   var name= $('#name ').val();
+   console.log(name);
+   $('h3').text(' Hello '+  name + "  Let's Learn Together  ");
+  }
 
-<body onload="showMessage()">
-
-</body>
+})
